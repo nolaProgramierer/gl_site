3fbf3bccae95380c49c9d70adc1ad673bddab137
Glenn Langdon <glennlangdon@gmail.com>
2016-06-22 11:33:28 -0500

    Comment model and controller working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,48 +1,50 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: [:show, :edit, :update,:destroy]
 
-  def index
-    @articles = Article.all
-  end
-
-  def show
-  end
-
-  def new
-    @article = Article.new
-  end
-
-  def edit
-  end
-
-  def create
-    @article = Article.create(article_params)
-    if @article.save
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    if @article.update(article_params)
-      redirect_to article_path(@article)
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @article.destroy
-    redirect_to articles_path
-  end
-
-    private
-    def article_params
-      params.require(:article).permit(:title, :text)
+    def index
+      @articles = Article.all
     end
 
-    def find_article
+    def show
       @article = Article.find(params[:id])
     end
-end
+
+    def new
+      @article = Article.new
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def create
+      @article = Article.new(article_params)
+
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+    end
+
+    def update
+      @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+
+      redirect_to articles_path
+    end
+
+    private
+      def article_params
+        params.require(:article).permit(:title, :text)
+      end
+  end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :find_article_id, only: [:create, :destroy]
+
+  def create
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
+    def find_article_id
+      @article = Article.find(params[:article_id])
+    end
 end
