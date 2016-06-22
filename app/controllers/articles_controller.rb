ce5c81bff9c2e0140e76a356c971909f5186c886
Glenn Langdon <glennlangdon@gmail.com>
2016-06-22 11:38:23 -0500

    Add before_action Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
     def index
       @articles = Article.all
     end
 
     def show
-      @article = Article.find(params[:id])
     end
 
     def new
@@ -13,12 +13,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-      @article = Article.find(params[:id])
     end
 
     def create
       @article = Article.new(article_params)
-
       if @article.save
         redirect_to @article
       else
@@ -27,8 +25,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-      @article = Article.find(params[:id])
-
       if @article.update(article_params)
         redirect_to @article
       else
@@ -37,14 +33,16 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-      @article = Article.find(params[:id])
       @article.destroy
-
       redirect_to articles_path
     end
 
     private
       def article_params
         params.require(:article).permit(:title, :text)
+      end
+
+      def find_article
+        @article = Article.find(params[:id])
       end
   end
