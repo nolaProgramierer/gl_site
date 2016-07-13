456b73a253eaba31e09a9f20a2083058d834b55f
Glenn Langdon <glennlangdon@gmail.com>
2016-07-13 11:14:44 -0500

    Attempt to nest replies from scratch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622045014) do
+ActiveRecord::Schema.define(version: 20160713152958) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160622045014) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "parent_id"
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
