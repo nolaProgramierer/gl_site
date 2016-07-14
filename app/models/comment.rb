f78ecbf4e8c84cd84c19a760556535327a3f8509
Glenn Langdon <glennlangdon@gmail.com>
2016-07-14 15:35:31 -0500

    Unsuccessful implemention of nested replies

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
-  belongs_to :parent, class_name: "Comment"
-  has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent,  class_name: "Comment" #-> requires "parent_id" column
+  has_many   :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
 end
