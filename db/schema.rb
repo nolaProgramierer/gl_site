e3e6d9bb59bb757fc9e4e3fc7e8782941d547efc
Glenn Langdon <glennlangdon@gmail.com>
2016-07-14 21:50:04 -0500

    Restored database from yesterday

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622045014) do
+ActiveRecord::Schema.define(version: 20160713220231) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20160622045014) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "ancestry"
   end
 
+  add_index "comments", ["ancestry"], name: "index_comments_on_ancestry"
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
