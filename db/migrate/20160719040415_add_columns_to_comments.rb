cfd25f13c66eedde29cd2a1bdf7a3ed8410dd877
Glenn Langdon <glennlangdon@gmail.com>
2016-07-18 23:17:57 -0500

    add name & email to Comment model

---
+++ db/migrate/20160719040415_add_columns_to_comments.rb
@@ -0,0 +1,6 @@
+class AddColumnsToComments < ActiveRecord::Migration
+  def change
+    add_column :comments, :email, :string
+    add_column :comments, :website, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715025555) do
+ActiveRecord::Schema.define(version: 20160719040415) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
+    t.text     "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
+    t.text     "commenter"
     t.text     "body"
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.string   "website"
   end
 
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
 end
