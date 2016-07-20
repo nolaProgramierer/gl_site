9dba66fe27b5cae343a125744d452a540cc24de8
Glenn Langdon <glennlangdon@gmail.com>
2016-07-19 22:38:09 -0500

    remove comment helpers, comment/form

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,17 +1,2 @@
 module ArticlesHelper
-  #method to change comment background for every other comment
-  def comment_list_bckgrd(count)
-    if (count.odd?)
-      bckgrd_color = "comment-list-wht"
-    else
-      bckgrd_color = "comment-list-offwht"
-    end
-    bckgrd_color
-  end
-
-  def show_no_comment
-    if (@article.comments.any?)
-      render 'comments'
-    end
-  end
 end
