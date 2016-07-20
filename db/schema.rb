e92b6239297e72d91c42f71c7c60bbb11b066529
Glenn Langdon <glennlangdon@gmail.com>
2016-07-19 22:48:11 -0500

    drop comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719233956) do
+ActiveRecord::Schema.define(version: 20160720034628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,16 +22,5 @@ ActiveRecord::Schema.define(version: 20160719233956) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "commenter"
-    t.text     "body"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "parent_id"
-  end
-
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
 end
