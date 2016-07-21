ef9e27b46cd7887384810adc0a74d18a1a000844
Glenn Langdon <glennlangdon@gmail.com>
2016-07-20 21:57:41 -0500

    Paperclip working w/ basic functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
 
     private
       def article_params
-        params.require(:article).permit(:title, :text)
+        params.require(:article).permit(:title, :text, :image)
       end
 
       def find_article
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 5 }
+  has_attached_file :image
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
