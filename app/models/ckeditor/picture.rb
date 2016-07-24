a2c8fba515baf7de5af29d3b9f358eb17650fe60
Glenn Langdon <glennlangdon@gmail.com>
2016-07-24 14:01:39 -0500

    another try

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -16,17 +16,22 @@ class Ckeditor::Picture < Ckeditor::Asset
 has_attached_file :data,
                   # url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
                   # path: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                  styles: { content: '800>', thumb: '118x100#' },
+                  # styles: { content: '800>', thumb: '118x100#' },
+                  # :storage => :s3,
+                  # :s3_credentials => "#{Rails.root}/config/aws.yml",
+                  # :bucket => 'glmusicsite'
+
                   :storage => :s3,
+                  :bucket => 'glmusicsite',
                   :s3_credentials => "#{Rails.root}/config/aws.yml",
-                  :bucket => 'glmusicsite'
+                  :styles => { :content => '800>', :thumb => '118x100#' }
 
 
-validates_attachment_presence :data
-validates_attachment_size :data, less_than: 2.megabytes
-validates_attachment_content_type :data, content_type: /\Aimage/
-
-def url_content
-  url(:content)
-end
+# validates_attachment_presence :data
+# validates_attachment_size :data, less_than: 2.megabytes
+# validates_attachment_content_type :data, content_type: /\Aimage/
+#
+# def url_content
+#   url(:content)
+# end
 end
