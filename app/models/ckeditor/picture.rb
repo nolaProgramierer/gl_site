5c46ff3b6b626b79f5995ed180337a2986e25ec6
Glenn Langdon <glennlangdon@gmail.com>
2016-07-24 13:31:43 -0500

    change ckedit to point to heroku

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,14 +1,32 @@
 class Ckeditor::Picture < Ckeditor::Asset
-  has_attached_file :data,
-                    url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                    path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                    styles: { content: '800>', thumb: '118x100#' }
+#   has_attached_file :data,
+#                     url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+#                     path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+#                     styles: { content: '800>', thumb: '118x100#' }
+#
+#   validates_attachment_presence :data
+#   validates_attachment_size :data, less_than: 2.megabytes
+#   validates_attachment_content_type :data, content_type: /\Aimage/
+#
+#   def url_content
+#     url(:content)
+#   end
+# end
 
-  validates_attachment_presence :data
-  validates_attachment_size :data, less_than: 2.megabytes
-  validates_attachment_content_type :data, content_type: /\Aimage/
+has_attached_file :data,
+                  url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+                  path: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+                  styles: { content: '800>', thumb: '118x100#' },
+                  :storage => :s3,
+                  :s3_credentials => "#{Rails.root}/config/aws.yml",
+                  :bucket => 'glmusicsite'
 
-  def url_content
-    url(:content)
-  end
+
+validates_attachment_presence :data
+validates_attachment_size :data, less_than: 2.megabytes
+validates_attachment_content_type :data, content_type: /\Aimage/
+
+def url_content
+  url(:content)
+end
 end
