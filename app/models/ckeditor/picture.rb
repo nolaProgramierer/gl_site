26b41f1cb11a446707959fcd686f037451cc6f21
Glenn Langdon <glennlangdon@gmail.com>
2016-07-25 14:27:07 -0500

    Revert "trying without aws-sdk"
    This reverts commit 6b408e5fcb05c68885898e5e7e5d512386a398ac.

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,32 +1,32 @@
 class Ckeditor::Picture < Ckeditor::Asset
-#   has_attached_file :data,
-#                     url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-#                     path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-#                     styles: { content: '800>', thumb: '118x100#' }
+  has_attached_file :data,
+                    url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+                    path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+                    styles: { content: '800>', thumb: '118x100#' }
+
+  validates_attachment_presence :data
+  validates_attachment_size :data, less_than: 2.megabytes
+  validates_attachment_content_type :data, content_type: /\Aimage/
+
+  def url_content
+    url(:content)
+  end
+end
+
+# has_attached_file :data,
+#                   url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+#                   path: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
 #
-#   validates_attachment_presence :data
-#   validates_attachment_size :data, less_than: 2.megabytes
-#   validates_attachment_content_type :data, content_type: /\Aimage/
+#                   storage: :s3,
+#                   s3_credentials: "#{Rails.root}/config/aws.yml",
+#                   bucket: 'glmusicsite',
+#                   styles: { content: '800>', thumb: '118x100#' }
 #
-#   def url_content
-#     url(:content)
-#   end
+# validates_attachment_presence :data
+# validates_attachment_size :data, less_than: 2.megabytes
+# validates_attachment_content_type :data, content_type: /\Aimage/
+#
+# def url_content
+#   url(:content)
 # end
-
-has_attached_file :data,
-                  url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                  path: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-
-                  storage: :s3,
-                  s3_credentials: "#{Rails.root}/config/s3.yml",
-                  bucket: 'glmusicsite',
-                  styles: { content: '800>', thumb: '118x100#' }
-
-validates_attachment_presence :data
-validates_attachment_size :data, less_than: 2.megabytes
-validates_attachment_content_type :data, content_type: /\Aimage/
-
-def url_content
-  url(:content)
-end
-end
+# end
