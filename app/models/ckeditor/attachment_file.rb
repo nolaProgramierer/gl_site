75e7465bbfc7a9a6bc1ec4f7adfdcfecfb7c122b
Glenn Langdon <glennlangdon@gmail.com>
2016-07-25 14:55:45 -0500

    added :bucket to ckeditor model

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -16,7 +16,8 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
                       url: '/ckeditor_assets/attachments/:id/:filename',
                       path: '/ckeditor_assets/attachments/:id/:filename',
-                      styles: { :content => '800>', :thumb => '118x100#' }
+                      styles: { :content => '800>', :thumb => '118x100#' },
+                      bucket: 'glmusicsite'
 
   validates_attachment_presence :data
   validates_attachment_size :data, less_than: 100.megabytes
