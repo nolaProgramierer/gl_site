4efb265346bd2f1ef94ef5bef26110988a650b26
Glenn Langdon <glennlangdon@gmail.com>
2016-07-25 15:45:45 -0500

    Heroku config

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,27 +1,8 @@
 class Ckeditor::Picture < Ckeditor::Asset
-  has_attached_file :data,
-                    url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                    path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                    styles: { content: '800>', thumb: '118x100#' }
-
-  validates_attachment_presence :data
-  validates_attachment_size :data, less_than: 2.megabytes
-  validates_attachment_content_type :data, content_type: /\Aimage/
-
-  def url_content
-    url(:content)
-  end
-end
-
-# for heroku remote
-# has_attached_file :data,
-#                   url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-#                   path: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-#                   storage: :s3,
-#                   bucket: 'glmusicsite',
-#                   :s3_credentials => "#{Rails.root}/config/s3.yml",
-#
-#                   styles: { content: '800>', thumb: '118x100#' }
+#   has_attached_file :data,
+#                     url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+#                     path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+#                     styles: { content: '800>', thumb: '118x100#' }
 #
 #   validates_attachment_presence :data
 #   validates_attachment_size :data, less_than: 2.megabytes
@@ -31,3 +12,20 @@ end
 #     url(:content)
 #   end
 # end
+
+for heroku remote
+has_attached_file :data,
+            url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+            path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+            styles: { content: '800>', thumb: '118x100#' },
+                  storage: :s3,
+                  :s3_credentials => "#{Rails.root}/config/aws.yml"
+
+  validates_attachment_presence :data
+  validates_attachment_size :data, less_than: 2.megabytes
+  validates_attachment_content_type :data, content_type: /\Aimage/
+
+  def url_content
+    url(:content)
+  end
+end
