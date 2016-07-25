163b3f5676bb25c1a07c0c026273c91ee3154df5
Glenn Langdon <glennlangdon@gmail.com>
2016-07-25 10:36:24 -0500

    paperclip ckedit config try

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -14,24 +14,19 @@ class Ckeditor::Picture < Ckeditor::Asset
 # end
 
 has_attached_file :data,
-                  # url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                  # path: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                  # styles: { content: '800>', thumb: '118x100#' },
-                  # :storage => :s3,
-                  # :s3_credentials => "#{Rails.root}/config/aws.yml",
-                  # :bucket => 'glmusicsite'
+                  url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+                  path: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
 
-                  :storage => :s3,
-                  :bucket => 'glmusicsite',
-                  :s3_credentials => "#{Rails.root}/config/aws.yml",
-                  :styles => { :content => '800>', :thumb => '118x100#' }
+                  storage: :s3,
+                  s3_credentials: "#{Rails.root}/config/aws.yml",
+                  bucket: 'glmusicsite',
+                  styles: { content: '800>', thumb: '118x100#' }
 
+validates_attachment_presence :data
+validates_attachment_size :data, less_than: 2.megabytes
+validates_attachment_content_type :data, content_type: /\Aimage/
 
-# validates_attachment_presence :data
-# validates_attachment_size :data, less_than: 2.megabytes
-# validates_attachment_content_type :data, content_type: /\Aimage/
-#
-# def url_content
-#   url(:content)
-# end
+def url_content
+  url(:content)
+end
 end
