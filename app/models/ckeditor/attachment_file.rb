9ce96f97d0fb36445e9ee681b54315ec0d0573c4
Glenn Langdon <glennlangdon@gmail.com>
2016-07-25 22:17:37 -0500

    return to orig ckedit config

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,35 +1,35 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
-#   has_attached_file :data,
-#                     url: '/ckeditor_assets/attachments/:id/:filename',
-#                     path: ':rails_root/public/ckeditor_assets/attachments/:id/:filename'
-#
-#   validates_attachment_presence :data
-#   validates_attachment_size :data, less_than: 100.megabytes
-#   do_not_validate_attachment_file_type :data
-#
-#   def url_thumb
-#     @url_thumb ||= Ckeditor::Utils.filethumb(filename)
-#   end
-# end
-
-# for heroku remote
   has_attached_file :data,
-                    # url: '/ckeditor_assets/attachments/:id/:filename',
-                    url: 'glmusicsite.s3.amazonaws.com',
-                    # path: ':rails_root/public/ckeditor_assets/attachments/:id/:filename',
-                    path: '/ckeditor_assets/attachments/:id/:filename',
-
-                    styles: { :content => '800>', :thumb => '118x100#' },
-                      storage: :s3,
-                      bucket: 'glmusicsite',
-                      s3_credentials:  '#{Rails.root}/config/aws.yml'
-
+                    url: '/ckeditor_assets/attachments/:id/:filename',
+                    path: ':rails_root/public/ckeditor_assets/attachments/:id/:filename'
 
   validates_attachment_presence :data
   validates_attachment_size :data, less_than: 100.megabytes
   do_not_validate_attachment_file_type :data
 
-def url_thumb
-      @url_thumb ||= Ckeditor::Utils.filethumb(filename)
-    end
+  def url_thumb
+    @url_thumb ||= Ckeditor::Utils.filethumb(filename)
+  end
 end
+
+# for heroku remote
+#   has_attached_file :data,
+#                     # url: '/ckeditor_assets/attachments/:id/:filename',
+#                     url: 'glmusicsite.s3.amazonaws.com',
+#                     # path: ':rails_root/public/ckeditor_assets/attachments/:id/:filename',
+#                     path: '/ckeditor_assets/attachments/:id/:filename',
+#
+#                     styles: { :content => '800>', :thumb => '118x100#' },
+#                       storage: :s3,
+#                       bucket: 'glmusicsite',
+#                       s3_credentials:  '#{Rails.root}/config/aws.yml'
+#
+#
+#   validates_attachment_presence :data
+#   validates_attachment_size :data, less_than: 100.megabytes
+#   do_not_validate_attachment_file_type :data
+#
+# def url_thumb
+#       @url_thumb ||= Ckeditor::Utils.filethumb(filename)
+#     end
+# end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.paperclip_defaults = {
-  #   storage: :s3,
-  #   s3_credentials: {
-  #     bucket: ENV.fetch('glmusicsite'),
-  #     access_key_id: ENV.fetch('AKIAIDDZQVDA6MWPGMVA'),
-  #     secret_access_key: ENV.fetch('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'),
-  #     s3_region: ENV.fetch('us-west-1'),
-  #   }
-  # }
 end
