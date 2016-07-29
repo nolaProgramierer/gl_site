3ecabee769a5809b807c8e83bc6ad0fee8e8d1bb
Glenn Langdon <glennlangdon@gmail.com>
2016-07-29 16:17:22 -0500

    commented out default paperclip config in env production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('gl-music-site'),
-    s3_region: ENV.fetch('us-west-2'),
-  }
-}
+#   config.paperclip_defaults = {
+#   storage: :s3,
+#   s3_credentials: {
+#     bucket: ENV.fetch('gl-music-site'),
+#     s3_region: ENV.fetch('us-west-2'),
+#   }
+# }
 
 
 end
