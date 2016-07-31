95312add311d103c30f788f85e0c53fb857e58ba
Glenn Langdon <glennlangdon@gmail.com>
2016-07-31 17:34:43 -0500

    change dkeditor models to orig config for testing on working DO server

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,20 +1,7 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
-#   has_attached_file :data,
-#                     url: '/ckeditor_assets/attachments/:id/:filename',
-#                     path: ':rails_root/public/ckeditor_assets/attachments/:id/:filename'
-#
-#   validates_attachment_presence :data
-#   validates_attachment_size :data, less_than: 100.megabytes
-#   do_not_validate_attachment_file_type :data
-#
-#   def url_thumb
-#     @url_thumb ||= Ckeditor::Utils.filethumb(filename)
-#   end
-# end
-
-has_attached_file :data,
+  has_attached_file :data,
                     url: '/ckeditor_assets/attachments/:id/:filename',
-                    path: '/home/rails/gl_site/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+                    path: ':rails_root/public/ckeditor_assets/attachments/:id/:filename'
 
   validates_attachment_presence :data
   validates_attachment_size :data, less_than: 100.megabytes
@@ -24,3 +11,16 @@ has_attached_file :data,
     @url_thumb ||= Ckeditor::Utils.filethumb(filename)
   end
 end
+
+# has_attached_file :data,
+#                     url: '/ckeditor_assets/attachments/:id/:filename',
+#                     path: '/home/rails/gl_site/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+#
+#   validates_attachment_presence :data
+#   validates_attachment_size :data, less_than: 100.megabytes
+#   do_not_validate_attachment_file_type :data
+#
+#   def url_thumb
+#     @url_thumb ||= Ckeditor::Utils.filethumb(filename)
+#   end
+# end
