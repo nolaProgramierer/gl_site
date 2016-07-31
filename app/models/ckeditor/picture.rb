6549cecb037bc4dab1b1fa82e1cea9d978d28937
Glenn Langdon <glennlangdon@gmail.com>
2016-07-31 17:39:50 -0500

    orig config for CKedit models; picture upload working

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -12,18 +12,3 @@ class Ckeditor::Picture < Ckeditor::Asset
     url(:content)
   end
 end
-
-
-# has_attached_file :data,
-#                   url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-#                   path: '/home/rails/gl_site/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-#                   styles: { content: '800>', thumb: '118x100#' }
-#
-# validates_attachment_presence :data
-# validates_attachment_size :data, less_than: 2.megabytes
-# validates_attachment_content_type :data, content_type: /\Aimage/
-#
-# def url_content
-#   url(:content)
-# end
-# end
