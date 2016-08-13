3384bb13fd6559fb99a453b40ea7be13cb8b4691
Glenn Langdon <glennlangdon@gmail.com>
2016-08-13 14:46:08 -0500

    saved attachment.rb model

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -19,10 +19,10 @@ has_attached_file :data,
                     :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :path => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :styles => { :content => '800>', :thumb => '118x100#' },
-                    :storage => :s3,
-                    :bucket => 'gl-music-site',
-                    :s3_region => 'us-west-2',
-                    :s3_credentials => "#{Rails.root}/config/aws.yml"
+                    # :storage => :s3,
+                    # :bucket => 'gl-music-site',
+                    # :s3_region => 'us-west-2',
+                    # :s3_credentials => "#{Rails.root}/config/aws.yml"
 validates_attachment_presence :data
 validates_attachment_size :data, less_than: 2.megabytes
 validates_attachment_content_type :data, content_type: /\Aimage/
