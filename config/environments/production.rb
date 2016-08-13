42289ddead56c7764463a87d732f1628252b290a
Glenn Langdon <glennlangdon@gmail.com>
2016-08-13 15:41:38 -0500

    delete aws.yml, add region to credentials hash

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,13 @@ Rails.application.configure do
 # }
 config.paperclip_defaults = {
     storage: :s3,
-    s3_region: ENV['us-east-1'],
+
     s3_credentials: {
       s3_host_name: ENV['s3.amazonaws.com'],
       bucket: ENV['gl-music-site'],
       access_key_id: ENV['AKIAITIPG6T7WY4DD7IQ'],
-      secret_access_key: ENV['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']
+      secret_access_key: ENV['5x+sDCLHTk/8U10YPD6Z6pEE15IwuXEoL83ycT5m'],
+      s3_region: ENV['us-east-1']
       }
     }
 end
