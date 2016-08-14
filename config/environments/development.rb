29e7fc67d9e3ff49cdfd180fe64754cf85891ac3
Glenn Langdon <glennlangdon@gmail.com>
2016-08-14 00:49:55 -0500

    added region to dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_host_name => 's3.amazonaws.com',
-  :bucket => 'gl-music-site'
+  :bucket => 'gl-music-site',
+  :s3_region => 'us-east-1'
 }
 end
