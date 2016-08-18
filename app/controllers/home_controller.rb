fb3b23b42debceecb5ebd3e6017e51dc4dcea429
Glenn Langdon <glennlangdon@gmail.com>
2016-08-18 16:01:19 -0500

    helper method to list links to 4 most recent blog entries in home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
 
   def index
-    @articles = Article.order("created_at DESC").limit(4).all
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,8 @@
 module HomeHelper
+
+  # find last 4 articles sorted by oldest to most recent for display in home page blog view
+  def article_helper(index)
+    @articles = Article.order('created_at DESC').first(4)
+    @article = @articles.fetch(index)
+  end
 end
