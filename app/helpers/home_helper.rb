6382b8adfa5796213d336c61dc46a38affea5b0c
Glenn Langdon <glennlangdon@gmail.com>
2016-08-19 20:40:39 -0500

    blog link truncate first of blog articles for links

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -6,9 +6,17 @@ module HomeHelper
     @article = @articles.fetch(index)
   end
 
+  # find title of last four articles in db
   def blog_title(index)
     @articles = Article.order('created_at DESC').first(4)
     @article = @articles.fetch(index)
     @article.title.html_safe
+  end
+
+  # return first 100 chars of the last four articles in db
+  def blog_text_teaser(index)
+    @articles = Article.order('created_at DESC').first(4)
+    @article = @articles.fetch(index)
+    truncate(strip_tags(@article.text), length: 100, separator: " ")
   end
 end
