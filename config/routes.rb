720fcc2d361daf4e455fc2b124c46402bde4fdff
Glenn Langdon <glennlangdon@gmail.com>
2016-08-28 13:56:48 -0500

    working Sign up for new users

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :users
+
   root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
