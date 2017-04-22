7ae58da6ca69b6a0b96faa4059e7d68aa874ac32
Glenn Langdon <glennlangdon@gmail.com>
2017-04-22 00:05:35 -0500

    Configured dotenv env vars, fixed Gemfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,10 +67,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    # user_name:             ENV["GMAIL_USERNAME"],
-    # password:              ENV["GMAIL_PASSWORD"],
-    user_name:             'glennlangdon@gmail.com',
-    password:              'cpwdirigentllfbfh327',
+    user_name:             ENV["GMAIL_USERNAME"],
+    password:              ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
